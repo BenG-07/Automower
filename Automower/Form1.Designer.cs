0acf4683fe2993bb82998c91f194ced6c578bbfd
BenG-07 <benjaminweirer@hotmail.com>
2022-11-04 01:10:47 +0100

    Initial commit

---
+++ Automower/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿//Author: Benjamin Weirer 5AHIF
+
+namespace Automower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(85, 50);
+            this.trackBar1.Maximum = 38;
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(85, 100);
+            this.trackBar2.Maximum = 20;
+            this.trackBar2.Minimum = 2;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 0;
+            this.trackBar2.Value = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(85, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 104);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bestätigen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Größe des Rasens:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Länge:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Breite:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 274);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Automower_Weirer5A";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Automower/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Automower
+{
+    public partial class Form1 : Form
+    {
+
+        public delegate void UpdateTextCallback(string text, int index);
+
+        public delegate void UpdateMowerCallback(int X, int Y);
+
+        public delegate int GetTrackBarCallback();
+
+        public delegate void SetPanelColorCallback(Color c, int index);
+
+        public List<Panel> pList = new List<Panel>();
+        public List<Label> lList = new List<Label>();
+        Mower m;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.ForestGreen;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.DarkRed;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar2.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(trackBar1);
+            Controls.Remove(trackBar2);
+            Controls.Remove(button1);
+            Controls.Remove(label1);
+            Controls.Remove(label2);
+            Controls.Remove(label3);
+            Controls.Remove(label4);
+            Controls.Remove(label5);
+
+            ProgressBar pb = new ProgressBar();
+            pb.Location = new Point(85, 158);
+            Controls.Add(pb);
+            for (int i = 0; i < 100; i++)
+            {
+                pb.Value = i;
+                System.Threading.Thread.Sleep(trackBar1.Value * trackBar2.Value / 10);
+            }
+            Controls.Remove(pb);
+
+            for (int i = 0; i < trackBar1.Value * trackBar2.Value; i++)
+            {
+                pList.Add(new Panel());
+                lList.Add(new Label());
+                lList[i].Text = "0";
+                lList[i].TextAlign = ContentAlignment.MiddleCenter;
+                lList[i].Width = lList[i].Width - 50;
+                pList[i].Width = 50;
+                pList[i].Height = 50;
+                pList[i].Location = new Point(12 + (i % trackBar1.Value) * 50, 12 + 50 * (i / trackBar1.Value));
+                pList[i].BorderStyle = BorderStyle.FixedSingle;
+                pList[i].Controls.Add(lList[i]);
+                Controls.Add(pList[i]);
+            }
+            lList[0].Text = "1";
+            pList[0].BackColor = Color.LawnGreen;
+            this.Width = trackBar1.Value * 50 + 40;
+            this.Height = trackBar2.Value * 50 + 62;
+
+            m = new Mower();
+            m.Location = new Point(12, 12);
+            m.BackColor = Color.Black;
+            m.Width = 50;
+            m.Height = 50;
+            Controls.Add(m);
+            m.BringToFront();
+
+            Thread th = new Thread(new ThreadStart(this.simulation));
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void simulation()
+        {
+            Random rng = new Random();
+            bool Everytilemowed = false, bumped = true;
+            int Winkel = 0, Lastbump = 0, lasttile = 0, currenttile = 0; //Lastbump 0=oben, 1=rechts, 2=unten, 3=links
+            double MowerLocationX = 12, MowerLocationY = 12;
+            while (!Everytilemowed)
+            {
+                if (bumped)
+                {
+                    if (rng.Next(0, 10) > 3)
+                        Winkel = rng.Next(1, 90);
+                    else
+                        Winkel = rng.Next(271, 360);
+                    bumped = false;
+                }
+                switch (Lastbump)
+                {
+                    case 0:
+                        if (Winkel <= 90)
+                        {
+                            if (!((MowerLocationX += Winkel / 100.0) + 50 < 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50 && (MowerLocationY += (90 - Winkel) / 100.0) + 50 < 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50))
+                            {
+                                bumped = true;
+                                if (MowerLocationX + 50 >= 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50)
+                                    Lastbump = 1;
+                                else
+                                    Lastbump = 2;
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (!((MowerLocationX -= (360 - Winkel) / 100.0) > 12 && (MowerLocationY += (Winkel - 270) / 100.0) + 50 < 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50))
+                            {
+                                bumped = true;
+                                if (MowerLocationX <= 12)
+                                    Lastbump = 3;
+                                else
+                                    Lastbump = 2;
+                                continue;
+                            }
+                        }
+                        break;
+                    case 1:
+                        if(Winkel <= 90)
+                        {
+                            if(!((MowerLocationX -= ((90 - Winkel) / 100.0)) > 12 && (MowerLocationY += Winkel / 100.0) + 50 < 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50))
+                            {
+                                bumped = true;
+                                if (MowerLocationY + 50 >= 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50)
+                                    Lastbump = 2;
+                                else
+                                    Lastbump = 3;
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (!((MowerLocationX -= ((Winkel - 270) / 100.0)) > 12 && (MowerLocationY -= (360 - Winkel) / 100.0) + 50 < 12))
+                            {
+                                bumped = true;
+                                if (MowerLocationY <= 12)
+                                    Lastbump = 0;
+                                else
+                                    Lastbump = 3;
+                                continue;
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (Winkel <= 90)
+                        {
+                            if (!((MowerLocationX -= Winkel / 100.0) > 12 && (MowerLocationY -= (90 - Winkel) / 100.0) > 12))
+                            {
+                                bumped = true;
+                                if (MowerLocationX <= 12)
+                                    Lastbump = 3;
+                                else
+                                    Lastbump = 0;
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (!((MowerLocationX += (360 - Winkel) / 100.0) + 50 < 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50 && (MowerLocationY -= (Winkel - 270) / 100.0) > 12))
+                            {
+                                bumped = true;
+                                if (MowerLocationX + 50 >= 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50)
+                                    Lastbump = 1;
+                                else
+                                    Lastbump = 0;
+                                continue;
+                            }
+                        }
+                        break;
+                    case 3:
+                        if(Winkel <= 90)
+                        {
+                            if(!((MowerLocationX += (90 - Winkel) / 100.0) + 50 < 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50 && (MowerLocationY -= Winkel / 100.0) > 12))
+                            {
+                                bumped = true;
+                                if (MowerLocationY <= 12)
+                                    Lastbump = 0;
+                                else
+                                    Lastbump = 1;
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (!((MowerLocationX += (360 - Winkel) / 100.0) + 50 < 12 + (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1)) * 50 && (MowerLocationY += (360 - Winkel) / 100.0) + 50 < 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50))
+                            {
+                                bumped = true;
+                                if (MowerLocationY >= 12 + (int)trackBar2.Invoke(new GetTrackBarCallback(this.GetTrackBar2)) * 50)
+                                    Lastbump = 2;
+                                else
+                                    Lastbump = 1;
+                                continue;
+                            }
+                        }
+                        break;
+                }
+                Thread.Sleep(10);
+                m.Invoke(new UpdateMowerCallback(this.UpdateMower), new object[] {(int)MowerLocationX, (int)MowerLocationY});
+                currenttile = (int)((MowerLocationX + 13) / 50) + ((int)(MowerLocationY + 13) / 50) * (int)trackBar1.Invoke(new GetTrackBarCallback(this.GetTrackBar1));
+                if(currenttile!=lasttile)
+                {
+                    lasttile = currenttile;
+                    lList[0].Invoke(new UpdateTextCallback(this.UpdateText),new object[] { (int.Parse(lList[lasttile].Text) + 1).ToString(), lasttile });
+                    pList[0].Invoke(new SetPanelColorCallback(this.SetPanelColor), new object[] { Color.LawnGreen, lasttile });
+                    Everytilemowed = checktiles();
+                }
+
+            }
+            int max = 0, index = 0;
+            for (int i = 0; i < lList.Count; i++)
+            {
+                if (int.Parse(lList[i].Text) > max)
+                {
+                    max = int.Parse(lList[i].Text);
+                    index = i;
+                }
+            }
+            pList[0].Invoke(new SetPanelColorCallback(this.SetPanelColor), new object[] { Color.Red, index });
+        }
+
+        private bool checktiles()
+        {
+            bool Everytilemowed = true;
+            foreach (var temp in lList)
+            {
+                if (int.Parse(temp.Text) < 10)
+                {
+                    Everytilemowed = false;
+                    break;
+                }
+            }
+            return Everytilemowed;
+        }
+
+        private void UpdateText(string text, int index)
+        {
+            lList[index].Text = text;
+        }
+
+        private void UpdateMower(int X, int Y)
+        {
+            m.Location = new Point(X, Y);
+        }
+
+        private int GetTrackBar1()
+        {
+            return trackBar1.Value;
+        }
+
+        private int GetTrackBar2()
+        {
+            return trackBar2.Value;
+        }
+
+        private void SetPanelColor(Color c, int index)
+        {
+            pList[index].BackColor = c;
+        }
+    }
+}
